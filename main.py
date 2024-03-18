95af3d3e006b9e5ab2e16161e8fa89ccdf834eaf
thonne <thonne.twange@turnkeyafrica.com>
2024-03-18 11:28:26 +0300

    Update code to use `snowflake-connector-python` instead of `snowflake.connector`.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import requests
 import pandas as pd
-from snowflake.connector import connect, Error
+import snowflake.connector
 from prefect import flow, task
 
 @task(log_prints=True, retries=3)
@@ -11,53 +11,55 @@ def fetch_data():
     return data['records']
 
 @task(log_prints=True, retries=3)
+def connect_to_snowflake():
+    try:
+        conn = snowflake.connector.connect(
+            user=os.getenv("snowflake_user"),
+            password=os.getenv("snowflake_password"),
+            account=os.getenv("snowflake_account"),
+            warehouse=os.getenv("snowflake_warehouse"),
+            database=os.getenv("snowflake_database"),
+            schema=os.getenv("snowflake_schema")
+        )
+        return conn
+    except Exception as e:
+        print(f"Error connecting to Snowflake: {e}")
+
+@task(log_prints=True, retries=3)
 def transformation(json_data):
     df = pd.DataFrame(json_data)
     df = df.drop(columns=["aFRR_ActivatedDK1", "aFRR_ActivatedDK2", "mFRR_ActivatedDK1", "mFRR_ActivatedDK2", "ImbalanceDK1", "ImbalanceDK2"])
     return df
 
 @task(log_prints=True, retries=3)
-def load(clean_data, snowflake_user, snowflake_password, snowflake_account, snowflake_warehouse, snowflake_database, snowflake_schema):
-    # Connect to Snowflake
-    conn = connect(
-        user=snowflake_user,
-        password=snowflake_password,
-        account=snowflake_account,
-        warehouse=snowflake_warehouse,
-        database=snowflake_database,
-        schema=snowflake_schema
-    )
-    cursor = conn.cursor()
-
-    clean_data_columns = clean_data.columns.tolist()
-    for _, row in clean_data.iterrows():
-        placeholders = ', '.join(['%s'] * len(clean_data_columns))
-        columns = ', '.join(clean_data_columns)
-        # Construct the SQL query
-        sql_query = f"""
-            INSERT INTO emission ({columns}) 
-            VALUES ({placeholders})
+def load(data, conn):
+    try:
+        snowflake_insert_sql = """
+            INSERT INTO emission (
+            Minutes1UTC, Minutes1DK, CO2Emission, ProductionGe100MW,
+            ProductionLt100MW, SolarPower, OffshoreWindPower,
+            OnshoreWindPower, Exchange_Sum, Exchange_DK1_DE,
+            Exchange_DK1_NL, Exchange_DK1_GB, Exchange_DK1_NO,
+            Exchange_DK1_SE, Exchange_DK1_DK2, Exchange_DK2_DE,
+            Exchange_DK2_SE, Exchange_Bornholm_SE
+        ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
         """
-        # Execute the SQL query
-        cursor.execute(sql_query, tuple(row))
 
-    conn.commit()
-    cursor.close()
-    conn.close()
-    print("Data loaded successfully into Snowflake.")
+        # Create a cursor from the Snowflake connection
+        cursor = conn.cursor()
+        cursor.executemany(snowflake_insert_sql, data)
+        cursor.close()
+        conn.commit()
+    except Exception as e:
+        print(f"Error inserting data to Snowflake: {e}")
+
 
 @flow(name="Energy API ingest")
 def energy_main():
-    snowflake_user = os.getenv("snowflake_user")
-    snowflake_password = os.getenv("snowflake_password")
-    snowflake_account = os.getenv("snowflake_account")
-    snowflake_warehouse = os.getenv("snowflake_warehouse")
-    snowflake_database = os.getenv("snowflake_database")
-    snowflake_schema = os.getenv("snowflake_schema")
-
     json_data = fetch_data()
+    conn = connect_to_snowflake()
     clean_data = transformation(json_data)
-    load(clean_data, snowflake_user, snowflake_password, snowflake_account, snowflake_warehouse, snowflake_database, snowflake_schema)
+    load(clean_data, conn)
 
 if __name__ == "__main__":
     energy_main()
